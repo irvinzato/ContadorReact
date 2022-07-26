b0ff6567b137ae90d02f6fa95582ae865edcdc1f
irvinzato <multizato@hotmail.com>
2022-07-26 14:42:48 -0500

    Ocupe hooks para renderizar componentes con su estado actual

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+//Hooks
+import { useState } from 'react';
 import './App.css';
+
 import yinYangLogo from './images/yinYang.png';
 import Button from './components/Button';
 import Counter from './components/Counter';
 
 function App() {
+  //Con los hooks puedo trabajar estados(variable click) dentro de componentes
+  //Ocupo el hook pasandole el valor inicial de "click" y con "setNumClicks" lo modificare dentro de las funciones
+  const [ click, setNumClicks ] = useState(0);
 
   const handleClick = () => {
     console.log('Hiciste click');
+    setNumClicks(click + 1);
   }
 
   const resetCounter = () => {
     console.log('Reinicio de contador');
+    setNumClicks(0);
   }
 
   return (
@@ -22,7 +30,7 @@ function App() {
       </div>
       <div className='counter-container'>
         <Counter
-          numClicks='x' />
+          numClicks={ click } />
         <Button 
           text='Click'
           buttonClick={ true }
